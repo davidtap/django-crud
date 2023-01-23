7c2d4d9749ed9a0ca4fd476e6f5f68386076e6e1
unknown <davidakle@gmail.com>
2023-01-22 20:04:30 -0500

    finality

--- tasks/forms.py
+++ tasks/forms.py
@@ -6,9 +6,9 @@ class TaskForm(forms.ModelForm):
     class Meta:
           model=Task
           fields = ['title','descripcion','important']
-          widgets = { #Se le da estilo al formulario html desde django
-                  'title': forms.TextInput (attrs={'class' : 'form-control', 'placeholder': 'Write a title'}),
-                  'descripcion': forms.Textarea (attrs={'class' : 'form-control', 'placeholder': 'Write a description'}),
-                  'important': forms.CheckboxInput (attrs={'class' : 'form-check-input'})
-          }
+          #widgets = { #Se le da estilo al formulario html desde django
+           #       'title': forms.TextInput (attrs={'class' : 'form-control', 'placeholder': 'Write a title'}),
+            #      'descripcion': forms.Textarea (attrs={'class' : 'form-control', 'placeholder': 'Write a description'}),
+             #     'important': forms.CheckboxInput (attrs={'class' : 'form-check-input'})
+         # }
 
